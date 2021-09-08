dee21f5219d6b00fcab583bb2fa5d53acd57b8ba
Rark7040 <crimera.hello@gmail.com>
2021-09-08 13:08:48 +0900

    仕様変更

--- src/rark/simple_economy/Account.php
+++ src/rark/simple_economy/Account.php
@@ -3,23 +3,25 @@ declare(strict_types = 1);
 
 namespace rark\simple_economy;
 
-use pocketmine\player\Player;
 use pocketmine\Server;
 
 class Account{
-
+	/** @var self[string] */
+	protected static array $instances = [];
 	protected string $name;
 
-	public function __construct($maybe_player){
-		if($maybe_player instanceof Player){
-			$this->name = $maybe_player->getName();
-
-		}else{
-			if(Server::getInstance()->getOfflinePlayer($maybe_player) === null){
+	public function __construct(string $name){
+		if(!isset(self::$instances[$name])){
+			if(Server::getInstance()->getOfflinePlayer($name) === null){
 				throw new \ErrorException('不正なアカウントが生成されました');
 			}
-			$this->name = $maybe_player;
+			$this->name = $name;
+			self::$instances[$this->name] = $this;
+
+		}else{
+			$this->name = $name;
 		}
+
 	}
 
 	public function getName():string{
